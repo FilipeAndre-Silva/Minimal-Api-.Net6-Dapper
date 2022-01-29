f49c1d641226b14f2dd2a30404d3156fb3b4b31c
Filipe André da Silva <filipeandre.private@gmail.com>
2022-01-29 11:47:12 -0300

    Implementação básica de uma API .NET 6 usando Dapper para acessar o banco SQL SERVER.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using TarefasApi.Endpoints;
+using TarefasApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddPersistence();
+
+var app = builder.Build();
+
+app.MapTarefasEndpoints();
+
+app.Run();
